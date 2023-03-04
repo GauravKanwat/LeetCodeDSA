ccacd53e4c1e2dd361bd24c6d846f1c8ceb91aee
gauravkanwat <gauravkanwat99@gmail.com>
2023-03-04 23:03:13 +0530

    More problems on BS

---
+++ src/BinarySearchProblems/SearchInRotatedArray.java
@@ -0,0 +1,49 @@
+//https://leetcode.com/problems/search-in-rotated-sorted-array
+package BinarySearchProblems;
+
+public class SearchInRotatedArray {
+    public static void main(String[] args) {
+        int[] arr = {1,3};
+        int target = 0;
+        System.out.println(search(arr, target));
+    }
+
+    static int search(int[] arr, int target) {
+        int pivot = pivotInRotatedArray(arr);
+        int result = binarySearch(arr, target, 0, pivot);
+        if(result != -1) {
+            return result;
+        }
+        else return binarySearch(arr, target, pivot+1, arr.length-1);
+    }
+
+    static int pivotInRotatedArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(mid < end && arr[mid] > arr[mid + 1])
+                return mid;
+            else if(mid > start && arr[mid] < arr[mid - 1])
+                return mid - 1;
+            else if(arr[start] > arr[mid])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return start;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(target == arr[mid])
+                return mid;
+            else if(target > arr[mid]) {
+                start = mid + 1;
+            }
+            else end = mid - 1;
+        }
+        return -1;
+    }
+}
